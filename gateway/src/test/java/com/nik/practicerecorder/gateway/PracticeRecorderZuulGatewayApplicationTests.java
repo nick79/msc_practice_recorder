a38bb9b51e3dd78aeeba26fdff37136e8737d93c
Milan <milnik79@gmail.com>
2020-10-23 11:01:08 +0200

    Added zuul gateway microservice.

---
+++ gateway/src/test/java/com/nik/practicerecorder/gateway/PracticeRecorderZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nik.practicerecorder.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRecorderZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
