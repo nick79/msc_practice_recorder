3055c009c1d4eedbb7366697e868a59db3737536
Milan <milnik79@gmail.com>
2020-10-23 09:51:52 +0200

    Added stub version of account microservice.

---
+++ account_management/src/main/java/com/nik/practicerecorder/api/account/PracticeRecorderApiAccountApplication.java
@@ -0,0 +1,13 @@
+package com.nik.practicerecorder.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRecorderApiAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRecorderApiAccountApplication.class, args);
+	}
+
+}
